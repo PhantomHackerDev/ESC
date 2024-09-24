6f3076316566ced57ad3956cbc5f3a6273b9943b
PhantomHackerDev <dennisfoos0719@outlook.com>
2024-09-24 19:25:31 -0400

    fix: remove unused variable

--- components/common/breadcrumb.tsx
+++ components/common/breadcrumb.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from "react";
-import { useRouter } from "next/router";
+//import { useRouter } from "next/router";
 import Link from "next/link";
 
 interface BreadcrumbItem {
--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
-import SearchBar from "@/components/common/searchBar";
+//import SearchBar from "@/components/common/searchBar";
 import Navbar from "@/components/common/navbar";
 
 const Header: React.FC = () => {
