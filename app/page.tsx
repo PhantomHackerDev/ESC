93347bf217e1be2042392ab74b6c871e59f0a4d8
PhantomHackerDev <dennisfoos0719@outlook.com>
2024-10-08 14:32:51 -0400

    fix : next to react

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Main from "@/components/page/main";
-
-export default function Home() {
-  return (
-    <div className="flex w-full bg-white">
-      <Main />
-    </div>
-  );
-}
--- utils/context.tsx
+++
@@ -1,5 +0,0 @@
-export const electron = [
-  'electronic_1.png',
-  'electronic_2.png',
-  'electronic_3.png',
-]
