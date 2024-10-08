441a8d1491674495135df79ef7e9fbc1b1c7e56d
PhantomHackerDev <dennisfoos0719@outlook.com>
2024-10-08 15:32:36 -0400

    fix: updeate develop branch

--- src/components/home/hero/index.js
+++ src/components/home/hero/index.js
@@ -79,30 +79,30 @@ const items = [
 function Hero() {
 
     return (
-        // <div className="hero">
-            <Carousel items={items} /> 
-        //     <div className='hero-content' data-aos='fade-down'>
-        //         <div className='sup'>
-        //             <div>ESC Hybrid Supercapacitor Battery</div>
-        //             <div>No Themal Runaway - No Explosion or Fire Risk</div>
-        //             <div>50,000+ Full Cycles at 100% DOD</div>
-        //             <div>20+ Year Lifespan</div>
-        //             <div>No Annual Upkeep or Maintenance</div>
-        //             <div>100% Fully Recyclable</div>
-        //         </div>
-        //         <div className='hero-description'>Electron Storage Corporation (ESC) delivers cutting-edge energy
-        //             storage technologies for a cleaner, sustainable world.</div>
-        //         <div className='hero-hidden-image'>
-        //             <img src={ESC_model} alt='hero-esc' className='hero-image' />
-        //         </div>
-        //         <div className='sub'>
-        //             <div className='button-c'>View Product</div>
-        //         </div>
-        //     </div>
-        //     <div className='hero-slide' data-aos='fade-down'>
-        //         <img src={ESC_model} alt='hero-esc' className='hero-image' />
-        //     </div>
-        // </div>
+        <div className="hero">
+            {/* <Carousel items={items} /> */}
+            <div className='hero-content' data-aos='fade-down'>
+                <div className='sup'>
+                    <div>ESC Hybrid Supercapacitor Battery</div>
+                    <div>No Themal Runaway - No Explosion or Fire Risk</div>
+                    <div>50,000+ Full Cycles at 100% DOD</div>
+                    <div>20+ Year Lifespan</div>
+                    <div>No Annual Upkeep or Maintenance</div>
+                    <div>100% Fully Recyclable</div>
+                </div>
+                <div className='hero-description'>Electron Storage Corporation (ESC) delivers cutting-edge energy
+                    storage technologies for a cleaner, sustainable world.</div>
+                <div className='hero-hidden-image'>
+                    <img src={ESC_model} alt='hero-esc' className='hero-image' />
+                </div>
+                <div className='sub'>
+                    <div className='button-c'>View Product</div>
+                </div>
+            </div>
+            <div className='hero-slide' data-aos='fade-down'>
+                <img src={ESC_model} alt='hero-esc' className='hero-image' />
+            </div>
+        </div>
     );
 }
 
